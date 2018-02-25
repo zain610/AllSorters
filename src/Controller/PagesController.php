ccb8e04015ff19daa7e1ee34fc0e3f8d1783976f
Peter Serwylo <peter.serwylo@monash.edu>
2018-02-25 11:20:57 +1100

    Change the home page from CakePHP template to info about a person.
    The info is hard coded in the pages controller. The routes file was
    changed to be more explicit (i.e. invoke the "pages" controller and
    "index" action). Removed now unused images that were pat of the original
    CakePHP template.

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -30,6 +30,23 @@ class PagesController extends AppController
 {
 
     /**
+     * Show a basic page with a persons name, and a short description of themselves.
+     * The information to be rendered is passed to the template from this action.
+     */
+    public function index()
+    {
+        $this->set('personName', 'Pete');
+        $this->set('favouriteColour', 'Green');
+        $this->set('favouriteColourValue', '#009900');
+
+        $goalForUnit = 'for students and staff to have fun, ' .
+            'while students learn as much as possible about working as a professional IT team.';
+
+        $this->set('goalForUnit', $goalForUnit);
+        $this->render('home');
+    }
+
+    /**
      * Displays a view
      *
      * @param array ...$path Path segments.
