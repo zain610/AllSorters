acd3d03edc479f60bf81ffbdce4e51b85ce6b886
Chaojie <cche0021@student.monash.edu>
2019-09-07 00:36:19 +1000

    2nd Commit

--- store.db.sql
+++ store.db.sql
@@ -1,46 +1,9 @@
-drop table if exists products cascade;
-
-create table products
+create table Client
 (
-	id int auto_increment
-		primary key,
-	name varchar(255) not null,
-	description varchar(1000) not null
+	Client_id int auto_increment,
+	Client_fname varchar(255) not null,
+	Client_sname varchar(255) not null,
+    constraint Client_pk
+		primary key (Client_id)
 );
-
-create table categories
-(
-    id int auto_increment,
-    name varchar(255) not null,
-    constraint categories_pk
-        primary key (id)
-);
-
-insert into products (id,name,description)
-values (1,'Orange juice',''),
-        (2,'Milo',''),
-        (3,'Sandwich','');
-
-insert into categories (id,name)
-values (1,'Food'),
-       (2,'Drink');
-
-create table categories_products
-(
-    product_id int not null,
-    category_id int not null,
-    constraint categories_products_pk
-        primary key (product_id, category_id),
-    constraint categories_products_categories_id_fk
-        foreign key (category_id) references categories (id),
-    constraint categories_products_products_id_fk
-        foreign key (product_id) references products (id)
-);
-
-
-insert into categories_products (product_id, category_id)
-values (1,2),
-       (2,1),
-       (2,2),
-       (3,1);
 
