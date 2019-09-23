4268a712dec8f63cac9416874c2f787dc605fb39
mgal0001 <mgal0001@student.monash.edu>
2019-09-23 20:45:08 +1000

    Archive functionality

--- src/Controller/Admin/BlogPostController.php
+++ src/Controller/Admin/BlogPostController.php
@@ -2,6 +2,7 @@
 namespace App\Controller\Admin;
 
 use App\Controller\AppController;
+use Cake\ORM\TableRegistry;
 
 /**
  * BlogPost Controller
@@ -152,5 +153,49 @@ class BlogPostController extends AppController
         }
 
         return $this->redirect(['action' => 'index']);
+    }
+    public function archive($id = null)
+    {
+        $blogPost = $this->BlogPost->get($id);
+        if ($blogPost == null) {
+            throw new NotFoundException();
+        }
+
+        // If an article is archived, it is "unpublished" as well
+        $blogPost->Archived = 1;
+        $blogPost->Published = 0;
+
+        if ($this->BlogPost->save($blogPost)) {
+            $this->Flash->success(__('Your Blog Post has been archived.'));
+        } else {
+            $this->Flash->error(__('Unable to archive your Blog Post.'));
+        }
+
+        return $this->redirect(['action' => 'index']);
+    }
+
+    public function restore($id = null)
+    {
+        $blogPost = $this->BlogPost->get($id);
+        if ($blogPost == null) {
+            throw new NotFoundException();
+        }
+
+        $blogPost->Archived = 0;
+        $blogPost->Published = 1;
+
+        if ($this->BlogPost->save($blogPost)) {
+            $this->Flash->success(__('Your Blog Post has been restored.'));
+        } else {
+            $this->Flash->error(__('Unable to restore your Blog Post.'));
+        }
+
+        return $this->redirect(['action' => 'archiveIndex']);
+    }
+    public function archiveIndex()
+    {
+        $this->layout ='admin';
+        $archivedBlogPosts = TableRegistry::get('BlogPost')->find('all')->where(['BlogPost.Archived' => 1])->contain([]);
+        $this->set('$archivedBlogPosts', $this->paginate($archivedBlogPosts));
     }
 }
