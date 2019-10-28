2a5991639d87425b6e677808c1bb0fddfe06c37b
Zain Shroff <zshr0001@student.monash.edu>
2019-10-29 02:46:58 +1100

    Fixed the emailing issue, the problem lies in the app.php which is not tracked by git due to gitignore and so had to copy the email config from app.php frm the server

--- src/Controller/Admin/SubscriptionsController.php
+++ src/Controller/Admin/SubscriptionsController.php
@@ -37,6 +37,8 @@ class SubscriptionsController extends AppController
         $subscriptions = $this->paginate($this->Subscriptions);
         $subscribers = $this->request->getSession()->read('subscribers');
         $this->set('subscribers', $subscribers);
+        $this->set('mail', $this->request->getSession()->read('mail'));
+        $this->set('email', $this->request->getSession()->read('sendermail'));
         $this->set(compact('subscriptions'));
     }
 
@@ -78,30 +80,6 @@ class SubscriptionsController extends AppController
     }
 
     /**
-     * Edit method
-     *
-     * @param string|null $id Subscription id.
-     * @return \Cake\Http\Response|null Redirects on successful edit, renders view otherwise.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function edit($id = null)
-    {
-        $subscription = $this->Subscriptions->get($id, [
-            'contain' => []
-        ]);
-        if ($this->request->is(['patch', 'post', 'put'])) {
-            $subscription = $this->Subscriptions->patchEntity($subscription, $this->request->getData());
-            if ($this->Subscriptions->save($subscription)) {
-                $this->Flash->success(__('The subscription has been saved.'));
-
-                return $this->redirect(['action' => 'index']);
-            }
-            $this->Flash->error(__('The subscription could not be saved. Please, try again.'));
-        }
-        $this->set(compact('subscription'));
-    }
-
-    /**
      * Delete method
      *
      * @param string|null $id Subscription id.
@@ -125,30 +103,36 @@ class SubscriptionsController extends AppController
         $this->layout = "admin";
         $this->request->allowMethod('post');
         //get the data from front end with the users to send the email to.
-        $subscribers = $this->request->getData();
+        $formData = $this->request->getData();
         $sender_list = [];
+        $message = "";
         //get the keys of the data = ids
-        $data_keys = array_keys($subscribers);
+        $data_keys = array_keys($formData); // [message, 1,2,3,4,5,6]
         //iterate over the keys list and foreach key, find the value is 1, then add it to the senders list
         foreach($data_keys as $key) {
-            if($subscribers[$key]) {
-                array_push($sender_list, $this->Subscriptions->find()->select(['email_address'])->where(['id' => $key])->toList());
+            if($key !== "message") {
+                if($formData[$key]) {
+                    array_push($sender_list, $this->Subscriptions->find()->select(['email_address'])->where(['id' => $key])->toList());
+                }
+            } else {
+                $message = $formData[$key];
             }
         }
-        $this->sendEmails($sender_list);
+        $this->sendEmails($sender_list, $message);
         $this->request->getSession()->write('subscribers', $sender_list );
         return $this->redirect(['action' => 'index']);
     }
-    private function sendEmails($sender_list = []) {
+    private function sendEmails($sender_list = [], $message = "") {
         //iterate over each sender and send an email.
         foreach ($sender_list as $sender) {
             $sender_email = $sender[0]['email_address'];
+            $this->request->getSession()->write('sendermail', $sender_email);
 
             $email = new Email('default');
             $email->setFrom(['allsortMary@gmail.com' => 'All Sorters'])
                 ->setTo($sender_email)
                 ->setTemplate('default')
-                ->setViewVars(['title' => "Test message from all sorters", 'content'=> '3000'])
+                ->setViewVars(['title' => "Newsletter update from AllSorters", 'content'=> strip_tags($message)])
                 ->setSubject("Test message #10000000");
 //            Email::deliver($sender_email, 'Hello World', 'Test message', ['from' => 'allsortMary@gmail.com']);
             if($email->send()) {
@@ -156,6 +140,7 @@ class SubscriptionsController extends AppController
             } else {
                 $this->request->getSession()->write('mail', false);
             }
+
         }
     }
 }
