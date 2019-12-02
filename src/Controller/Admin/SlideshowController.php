cce328f06272e0cda4c8f8b28eb450067b44ce02
Chaojie Chen <cche0021@student.monash.edu>
2019-12-03 00:27:49 +1100

    slideshow front end display and back end management

--- src/Controller/Admin/SlideshowController.php
+++ src/Controller/Admin/SlideshowController.php
@@ -2,6 +2,7 @@
 namespace App\Controller\Admin;
 
 use App\Controller\AppController;
+use App\Model\Entity\Image;
 
 /**
  * Slideshow Controller
@@ -91,8 +92,9 @@ class SlideshowController extends AppController
      */
     public function edit($id = null)
     {
+        $this->layout ='admin';
         $slideshow = $this->Slideshow->get($id, [
-            'contain' => []
+            'contain' => ['Image']
         ]);
         if ($this->request->is(['patch', 'post', 'put'])) {
             $slideshow = $this->Slideshow->patchEntity($slideshow, $this->request->getData());
