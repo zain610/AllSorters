506ddd10fb48acc637c02bcfc1a8367075170ae1
dlii0012 <dlii0012@student.monash.edu>
2020-02-01 20:35:26 +1100

    replace the <link> and <script> tags on admin/index.ctp

--- tests/Fixture/AboutFixture.php
+++ tests/Fixture/AboutFixture.php
@@ -5,18 +5,15 @@ use Cake\TestSuite\Fixture\TestFixture;
 
 /**
  * AboutFixture
- *
  */
 class AboutFixture extends TestFixture
 {
-
     /**
      * Table name
      *
      * @var string
      */
     public $table = 'about';
-
     /**
      * Fields
      *
@@ -26,9 +23,14 @@ class AboutFixture extends TestFixture
     public $fields = [
         'about_id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'autoIncrement' => true, 'precision' => null],
         'Title' => ['type' => 'string', 'length' => 255, 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
-        'Content' => ['type' => 'string', 'length' => 255, 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
+        'Content' => ['type' => 'text', 'length' => null, 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => '', 'precision' => null],
+        'image_id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => true, 'default' => null, 'comment' => '', 'precision' => null, 'autoIncrement' => null],
+        '_indexes' => [
+            'about_image_Image_id_fk' => ['type' => 'index', 'columns' => ['image_id'], 'length' => []],
+        ],
         '_constraints' => [
             'primary' => ['type' => 'primary', 'columns' => ['about_id'], 'length' => []],
+            'about_image_Image_id_fk' => ['type' => 'foreign', 'columns' => ['image_id'], 'references' => ['image', 'Image_id'], 'update' => 'restrict', 'delete' => 'restrict', 'length' => []],
         ],
         '_options' => [
             'engine' => 'InnoDB',
@@ -36,17 +38,21 @@ class AboutFixture extends TestFixture
         ],
     ];
     // @codingStandardsIgnoreEnd
-
     /**
-     * Records
+     * Init method
      *
-     * @var array
+     * @return void
      */
-    public $records = [
-        [
-            'about_id' => 1,
-            'Title' => 'Lorem ipsum dolor sit amet',
-            'Content' => 'Lorem ipsum dolor sit amet'
-        ],
-    ];
+    public function init()
+    {
+        $this->records = [
+            [
+                'about_id' => 1,
+                'Title' => 'Lorem ipsum dolor sit amet',
+                'Content' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
+                'image_id' => 1
+            ],
+        ];
+        parent::init();
+    }
 }
